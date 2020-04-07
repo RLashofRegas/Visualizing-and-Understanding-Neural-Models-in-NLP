6ca2e7717b2ae403203bb1a6e85c76974eeea6b1
Robin Lashof-Regas <Robin.Lashof@gmail.com>
2020-04-06 23:43:22 -0700

    make heatmap save new files each time

--- heatmap.py
+++ heatmap.py
@@ -1,5 +1,22 @@
 import numpy
 import matplotlib.pylab as plt
+from os import listdir
+
+def getNextFilename():
+    filename_base = 'heatmap'
+    plots_dir = 'plots'
+    filename_int_length = 2
+
+    heatmap_files = [file for file in listdir(plots_dir) if file.startswith(filename_base)]
+    if not heatmap_files:
+        next_int = 0
+    else:
+        heatmap_files.sort()
+        next_int = int(heatmap_files[-1][-6:-4]) + 1
+
+    next_int_str = str(next_int).zfill(filename_int_length)
+    return plots_dir + '/' + filename_base + '-' + next_int_str + '.png'
+    
 
 matrix = numpy.loadtxt("matrix")
 matrix_magnify=numpy.zeros((matrix.shape[0]*10,matrix.shape[1]))
@@ -10,4 +27,4 @@ fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 plt.imshow(matrix_magnify, interpolation='nearest', cmap=plt.cm.Blues)
 plt.colorbar()
-plt.savefig('./plots/heatmap.png')
+plt.savefig(getNextFilename())
