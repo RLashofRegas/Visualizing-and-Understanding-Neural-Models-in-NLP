e5541fdfb3c16c1a45ac284c952358cafbbfbdab
Robin Lashof-Regas <Robin.Lashof@gmail.com>
2020-04-11 09:27:43 -0700

    fix shell scripts

--- saliency_derivative.sh
+++ saliency_derivative.sh
@@ -1,8 +1,8 @@
 if [ -z ${1+x} ]; 
 then 
-    MODEL = "model";
+    MODEL="model";
 else 
-    MODEL = $1;
+    MODEL=$1;
 fi
 
 python util/StringToNum.py data/dict.txt input.txt util/input_index.txt
