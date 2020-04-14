d3b991f682b37dd057453efcdf0461d26a5808af
Robin Lashof-Regas <Robin.Lashof@gmail.com>
2020-04-13 21:28:14 -0700

    add more comments

--- sentiment_bidi/data.lua
+++ sentiment_bidi/data.lua
@@ -1,14 +1,19 @@
 require "torchx"
 local stringx = require('pl.stringx')
 local function fromString(str,splitter)
+    -- split string on splitter chars
     local split=stringx.split(str,splitter);
     local Return={};
+    -- tensors of 0 of number of elements - 1 (first element is class label)
     local tensor=torch.Tensor(1,#split-1):zero()
     local tensor_r=torch.Tensor(1,#split-1):zero()
     for i=2,#split do
+        -- word indices
         tensor[1][i-1]=tonumber(split[i]);
+        -- word indices in reverse
         tensor_r[1][#split-i+1]=tonumber(split[i]);
     end
+    -- return {class label, phrase, reverse phrase}
     return {tonumber(split[1]),tensor,tensor_r};
 end
 
@@ -42,20 +47,24 @@ end
 
 
 local function get_batch(texts,texts_r)
+    -- get max length of phrases
     local max_length=-100;
     for i=1,#texts do   
         if texts[i]:size(2)>max_length then
             max_length=texts[i]:size(2)
         end
     end
+
     local Words=torch.Tensor(#texts,max_length):fill(1);
     local Words_r=torch.Tensor(#texts,max_length):fill(1);
     local mask=torch.Tensor(#texts,max_length):zero();
     for i=1,#texts do
+        -- get row for this phrase, and only the last N columns where N is number of words
         Words:sub(i,i,max_length-texts[i]:size(2)+1,max_length):copy(texts[i]);
         Words_r:sub(i,i,max_length-texts[i]:size(2)+1,max_length):copy(texts_r[i]);
         mask:sub(i,i,max_length-texts[i]:size(2)+1,max_length):copy(texts[i]);
     end
+    -- fill Mask - one row for each column containing the indexes of all zeros
     local Mask={};
     for i=1,Words:size(2) do
         Mask[i]=torch.LongTensor(torch.find(mask:sub(1,-1,i,i),0))
@@ -69,20 +78,31 @@ local function read_train(open_train_file,batch_size)
     End=0;
     while 1==1 do
         i=i+1;
+        -- read next line from io (needs to be set to train file prior to calling this function)
         local str=io.read();
+        -- break at the end of the file
         if str==nil then
             End=1
             break;
         end
+        -- get {class label, phrase indices, reverse phrase indices}
+        -- phrases are 1 row tensors with #columns = number of words
         local Split=fromString(str);
+        -- class labels
         Y[i]=Split[1];
+        -- forwards phrases
         texts[i]=Split[2];
+        -- reverse phrases
         texts_r[i]=Split[3];
+        -- only read one batch
         if i==batch_size then
             break;
         end
     end
+    -- Words/Words_r - tensor of width max_length with word indices at the end of the row
+    -- Mask - one row for each column of Words/Words_r with indices of all zeros.
     local Words,Words_r,Mask=get_batch(texts,texts_r);
+    -- return whether we finished the file, tensor of class labels, tensors of word indices (forwards and backwards), indices of zeros
     return End,torch.Tensor(Y),Words,Words_r,Mask
 end
 
--- sentiment_bidi/main.lua
+++ sentiment_bidi/main.lua
@@ -48,6 +48,8 @@ end
 
 
 local function lstm(x,prev_h,prev_c)
+    -- x = each row is a word from a phrase embedded in "dimension" dimensions
+
     -- dropout weights from the input
     local drop_x=nn.Dropout(params.dropout)(x)
     -- dropout weights from the output of the previous layer
@@ -55,14 +57,13 @@ local function lstm(x,prev_h,prev_c)
     -- dim x 4*dim linear layers on x and h (concatenate to create input gate)
     local i2h=nn.Linear(params.dimension,4*params.dimension)(drop_x);
     local h2h=nn.Linear(params.dimension,4*params.dimension)(drop_h);
-    -- add tensor ouputs of i2h and h2h = 1 4*dim dimensional tensor
+    -- add tensor ouputs of i2h and h2h = n_phrases x 4*dim dimensional tensor
     local gates=nn.CAddTable()({i2h,h2h});
-    -- reshape into 4 rows and "dimension" columns
-    -- !! THIS SEEMS WRONG I THINK IT"S BACKWARDS !!
+    -- reshape each row (current word from each phrase) into 4xdimension Tensor = n_phrasesx4xdimension
     local reshaped_gates =  nn.Reshape(4,params.dimension)(gates);
-    -- split each column of the reshaped_gates into separate tensors (dimension tensors of length 4)
+    -- split each of the 4 gates into separate tensors, 4 tensors of dim = n_phrasesxdimension
     local sliced_gates = nn.SplitTable(2)(reshaped_gates);
-    -- get first 4 columns as gates
+    -- get each of the 4 tensors as the gates
     local in_gate= nn.Sigmoid()(nn.SelectTable(1)(sliced_gates))
     local in_transform= nn.Tanh()(nn.SelectTable(2)(sliced_gates))
     local forget_gate= nn.Sigmoid()(nn.SelectTable(3)(sliced_gates))
@@ -76,10 +77,12 @@ local function lstm(x,prev_h,prev_c)
 end
 
 local function encoder_()
+    -- x_index is passed in as one word for each
     local x_index=nn.Identity()();
     local prev_c=nn.Identity()();
     local prev_h=nn.Identity()();
     -- 60 dimensional embedding vectors, one for each word in vocab
+    -- in model returns tensor with 1 row per phrase, row is embedding vector of current word
     local x=LookupTable(params.vocab_size,params.dimension)(x_index);
     -- get graph nodes for computing next_h and next_c
     next_h,next_c=lstm(x,prev_h,prev_c)
@@ -110,6 +113,7 @@ local function softmax_()
     local err= nn.ClassNLLCriterion()({pred, y})
     -- compile module
     local module= nn.gModule({h_left,h_right,y},{err,pred});
+
     -- init params to uniform distribution
     module:getParameters():uniform(-params.init_weight, params.init_weight)
     -- transfer module to gpu
@@ -117,8 +121,11 @@ local function softmax_()
 end 
 
 local function forward(Word,Word_r,Delete,isTraining)
+    -- transfer word-index tensor to GPU
     Word=Word:cuda()
+    -- for each word in the phrases
     for t=1,Word:size(2) do
+        -- initialize h and c from the last batch (0's if it's the first batch)
         if t==1 then
             h_t_1=torch.zeros(Word:size(1),params.dimension):cuda();
             c_t_1=torch.zeros(Word:size(1),params.dimension):cuda();
@@ -126,18 +133,29 @@ local function forward(Word,Word_r,Delete,isTraining)
             h_t_1=model.h_left[t-1];
             c_t_1=model.c_left[t-1];
         end
+
+        -- prev_h, prev_c, t'th word of for each phrase
         inputs={h_t_1,c_t_1,Word:select(2,t)}
+
+        -- during training dropout is used, during evaluate it's not
         if isTraining then
             model.lstms_left[t]:training();
         else
             model.lstms_left[t]:evaluate();
         end
+
+        -- get next_h, next_c from model
         model.h_left[t],model.c_left[t]=unpack(model.lstms_left[t]:forward(inputs))
+
+        -- if there are phrases in Words that have a zero for this column
         if Delete[t]:nDimension()~=0 then
+            -- copy in the zeroes for those phrases
             model.h_left[t]:indexCopy(1,Delete[t],torch.zeros(Delete[t]:size(1),params.dimension):cuda())
             model.c_left[t]:indexCopy(1,Delete[t],torch.zeros(Delete[t]:size(1),params.dimension):cuda())
         end
     end
+
+    -- same as above but for reverse phrases
     Word=Word_r:cuda()
     for t=1,Word:size(2) do
         if t==1 then
@@ -153,6 +171,7 @@ local function forward(Word,Word_r,Delete,isTraining)
         else
             model.lstms_right[t]:evaluate();
         end
+
         model.h_right[t],model.c_right[t]=unpack(model.lstms_right[t]:forward(inputs))
         if Delete[t]:nDimension()~=0 then
             model.h_right[t]:indexCopy(1,Delete[t],torch.zeros(Delete[t]:size(1),params.dimension):cuda())
@@ -162,8 +181,12 @@ local function forward(Word,Word_r,Delete,isTraining)
 end
 
 local function backward(Word,Delete,dh_left,dh_right)
+    -- zero initial gradient for c
     local dc_left=torch.zeros(Word:size(1),params.dimension):cuda();
+    -- iterate backwards through dimension
     for t=Word:size(2),1,-1 do
+
+        -- get output from previous layer (input to this layer)
         if t==1 then
             h_t_1=torch.zeros(Word:size(1),params.dimension):cuda();
             c_t_1=torch.zeros(Word:size(1),params.dimension):cuda();
@@ -171,12 +194,18 @@ local function backward(Word,Delete,dh_left,dh_right)
             h_t_1=model.h_left[t-1];
             c_t_1=model.c_left[t-1];
         end
+
+        -- get gradients
         dh_left,dc_left=unpack(model.lstms_left[t]:backward({h_t_1,c_t_1,Word:select(2,t)},{dh_left,dc_left}));
+
+        -- zero out gradients for phrases that aren't long enough for this index
         if Delete[t]:nDimension()~=0 then
             dh_left:indexCopy(1,Delete[t],torch.zeros(Delete[t]:size(1),params.dimension):cuda())
             dc_left:indexCopy(1,Delete[t],torch.zeros(Delete[t]:size(1),params.dimension):cuda())
         end
     end
+
+    -- same as above for reverse direction
     local dc_right=torch.zeros(Word:size(1),params.dimension):cuda();
     for t=Word:size(2),1,-1 do
         if t==1 then
@@ -195,6 +224,7 @@ local function backward(Word,Delete,dh_left,dh_right)
 end
 
 local function test(filename)
+    -- open file and set as default input
     open_train_file=io.open(filename,"r")
     io.input(open_train_file)
     local End,Y,Word,Word_r,Delete;
@@ -202,15 +232,22 @@ local function test(filename)
     End=0;
     local right=0;
     local total=0;
+    -- loop through file in minibatches
     while End==0 do
+        -- get indexed phrases
         End,Y,Word,Word_r,Delete=data.read_train(open_train_file,params.batch_size);
         Y=Y:cuda()
         if End==1 then 
             break;
         end
+        -- forward through model for prediction
         forward(Word,Word_r,Delete,false);
         err,pred=unpack(softmax:forward({model.h_left[Word:size(2)],model.h_right[Word:size(2)],Y:cuda()}))
+
+        -- get prediction
         local score,prediction=torch.max(pred,2)
+
+        -- evaluate prediction against actual
         prediction:resize(prediction:size(1)*prediction:size(2));
         total=total+prediction:size(1);
         for i=1,prediction:size(1) do
@@ -219,6 +256,8 @@ local function test(filename)
             end
         end
     end
+
+    -- return accuracy
     local accuracy=right/total;
     return accuracy;
 end
@@ -237,14 +276,17 @@ encoder_right =encoder_()
 -- outputs: error, predicted class probabilities
 softmax=softmax_();
 
+-- make sure parameters are flattened
 encoder_left:getParameters()
 encoder_right:getParameters()
 softmax:getParameters()
 
+-- get params and gradients
 paramx[1],paramdx[1]=encoder_left:parameters()
 paramx[2],paramdx[2]=encoder_right:parameters()
 paramx[3],paramdx[3] =softmax:parameters()
 
+-- initialize ada - same shape as parameters with randomized epsilons
 for i=1,3 do
     ada[i]={};
     for j=1,#paramx[i] do
@@ -264,14 +306,16 @@ model.c_right={};
 local timer=torch.Timer();
 --embedding=data.read_embedding()
 
+-- make left and right lstms use the same initial params
 paramx[2][1]:copy(paramx[1][1])
 
-
+-- get N copies of the lstm as array
 model.lstms_left=g_cloneManyTimes(encoder_left,params.max_length)
 model.lstms_right=g_cloneManyTimes(encoder_right,params.max_length)
 
 iter=0;
 
+-- store starting parameters
 store_param={};
 for i=1,#paramx do
     store_param[i]={}
@@ -284,25 +328,47 @@ local best_accuracy=-1;
 
 while iter<params.max_iter do
     iter=iter+1;
+
+    -- open and set the training file as standard input
     open_train_file=io.open(params.train_file,"r")
     io.input(open_train_file)
+
     local End,Y,Word,Delete;
     End=0;
     local time1=timer:time().real;
+    -- loop through train file in mini batches
     while End==0 do
+        -- zero out gradients
         for i=1,#paramdx do
             for j=1,#paramdx[i] do
                 paramdx[i][j]:zero();
             end
         end
+
+        -- End = whether we finished reading the file
+        -- Y = tensor of class labels
+        -- Word/Word_r = tensor with word indices (forwards/reverse) at the end of each row
+        -- Delete = for each column, indices of zeros (rows that don't have a word in that position)
         End,Y,Word,Word_r,Delete=data.read_train(open_train_file,params.batch_size);
+
+        -- doesn't this throw out the last partial batch?
         if End==1 then 
             break;
         end
+
+        -- evaluate each phrase in batch
         forward(Word,Word_r,Delete,true)
+
+        -- get error and probs of predicted classes
         err,pred=unpack(softmax:forward({model.h_left[Word:size(2)],model.h_right[Word:size(2)],Y:cuda()}))
+
+        -- get initial dh's
         local dh_left,dh_right=unpack(softmax:backward({model.h_left[Word:size(2)],model.h_right[Word:size(2)],Y:cuda()},{torch.ones(1):cuda(),torch.zeros(Word:size(1),5):cuda() }))
+
+        -- backprop on main LSTMs
         backward(Word,Delete,dh_left,dh_right)
+
+        -- update parameters using adagrad
         for i=1,#ada do
             for j=1,#ada[i] do
                 if i==2 and j==1 then
@@ -313,9 +379,6 @@ while iter<params.max_iter do
             end
         end
         paramx[1][1]:copy(paramx[2][1])
-        --print(paramdx[1])
-        --ada[2]=ada[2]:add(paramdx[2]:sqrt())
-        --print(paramx[1])       
     end
     local time2=timer:time().real;
     acc_dev=test(params.dev_file)
@@ -333,6 +396,7 @@ while iter<params.max_iter do
     end
 end
 
+-- output model to file storing best params found
 local getNextFileName = require('../get-next-filename.lua')
 local modelFileName = getNextFileName('model', '', '.', 2)
 local file=torch.DiskFile(modelFileName,"w"):binary();
